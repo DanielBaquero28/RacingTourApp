dd409146e9917083400453d8e7fee46df83a2ea0
Daniel Alejandro Baquero <835@holbertonschool.com>
2020-08-18 15:39:27 -0500

    [Feature]: Testing v9

--- FinishCamera.cs
+++ FinishCamera.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 // In charge of managing the camera when user has won
 public class FinishCamera : MonoBehaviour
 { 
